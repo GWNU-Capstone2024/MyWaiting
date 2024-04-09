45f06e074e9494f12706f5f8d3be2bb3efda3351
Mascari4615 <mascari4615@gmail.com>
2024-04-09 13:41:18 +0900

    Init

---
+++ src/main/java/com/gwnu2024/mywaiting/MyWaitingApplication.java
@@ -0,0 +1,13 @@
+package com.gwnu2024.mywaiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWaitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWaitingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwnu2024/mywaiting/MyWaitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwnu2024.mywaiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWaitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
